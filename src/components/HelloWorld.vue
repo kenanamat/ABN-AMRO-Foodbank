d7fa02a3fd9aa4cedd58c7b581c44d748b56c6dc
Kongonon <33282458+kenanamat@users.noreply.github.com>
2023-07-27 16:41:26 +0200

    Initial project setup :tada:

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue"
 
 defineProps<{ msg: string }>()
 
@@ -7,7 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-3xl text-red-700 bg-emerald-700">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -30,9 +30,3 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
